f638f2bbf9ff0cfa6809ba88aa90102b69d5bfca
Biswa <sbiswabhusan13@gmail.com>
2026-05-16 23:28:43 +0530

    about to implement the visitor pattern(professional way) and introduced the various expression and statement nodes and all have the identity of AST NODE(parent template) using inheritance so that don;t have to include lot of unique pointers inside the print statement and similar reasons etc.

--- parser/AST.h
+++ parser/AST.h
@@ -5,36 +5,33 @@
 #include <vector>
 #include "../lexer/Token.h"
 
-// ---------------------------------------------------------
-// TODO 1: The Visitor Pattern (High-Level Architecture)
-// To keep your compiler professional and modular, use the Visitor Pattern.
-// This allows you to separate the AST node definitions from the logic
-// that operates on them (like printing the tree, or compiling it).
-// ---------------------------------------------------------
+// Forward declarations so classes know about each other
 class ASTVisitor;
+class BinaryExpr;
+class LiteralExpr;
+class VariableExpr;
+class PrintStmt;
+class VarDecl;
+class BlockStmt;
+class IfStmt;
+class WhileStmt;
+
+// IMPLEMENTING THE VISITOR PATTERN(A professional  way of designing compilers)(good designing)
+//let's say u have a number of nodes inorder to do a thing suppose print tree or compile or find error u would have to edit each of the node this creates a mess
+// rather u make all the nodes dumb that is no logic only data and then create a visitor class for the required purpose
 
 class ASTNode {
 public:
-    virtual ~ASTNode() = default;
+    virtual ~ASTNode() = default;  //virtual destructor in order to prevent memory leaks
     
     // Every node must accept a visitor
     virtual void accept(ASTVisitor& visitor) = 0;
 };
 
-// ---------------------------------------------------------
-// TODO 2: Define specific AST Node classes inheriting from ASTNode.
-//
-// Expression Nodes: BinaryExpr, LiteralExpr, VariableExpr
-// Statement Nodes: PrintStmt, VarDecl, BlockStmt, IfStmt, WhileStmt
-// ---------------------------------------------------------
 
-// Example structure for the Visitor Interface:
-class ASTVisitor {
-public:
-    // virtual void visit(BinaryExpr* expr) = 0;
-    // virtual void visit(LiteralExpr* expr) = 0;
-    // virtual void visit(PrintStmt* stmt) = 0;
-    virtual ~ASTVisitor() = default;
-};
+// Define specific AST Node classes inheriting from ASTNode.
+
+
+
 
 #endif // AST_H
