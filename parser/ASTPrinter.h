3319ef87e09ee3af67111cfd7e638febef2e6cb2
Biswa <sbiswabhusan13@gmail.com>
2026-05-17 22:06:08 +0530

    Updated ASTPrinter.h

--- parser/ASTPrinter.h
+++ parser/ASTPrinter.h
@@ -18,6 +18,7 @@ public:
     void visit(BinaryExpr* expr) override;
     void visit(LiteralExpr* expr) override;
     void visit(VariableExpr* expr) override;
+    void visit(AssignExpr* expr) override;
     void visit(PrintStmt* stmt) override;
     void visit(VarDecl* stmt) override;
     void visit(BlockStmt* stmt) override;
