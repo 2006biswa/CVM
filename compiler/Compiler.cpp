5734055ea5b6eb8c77c1df10ebcf2ca9e94fde9b
Biswa <sbiswabhusan13@gmail.com>
2026-05-17 15:24:20 +0530

    Created the compile function and also realised that till now we have not implemented the assignment expression such as x=10;

--- compiler/Compiler.cpp
+++ compiler/Compiler.cpp
@@ -1,26 +1,39 @@
 #include "Compiler.h"
+#include <iostream>
+
+//SETUP
 
 Compiler::Compiler() {
-    // ---------------------------------------------------------
-    // TODO: Initialize compiler state.
-    // ---------------------------------------------------------
+// Nothing special needed to initialize yet
+}
+// main.cpp handles the root node of the AST usually the BLOCKSTMT to the  compile function
+
+Chunk Compiler::compile(ASTNode* ast) {
+    
+    currentChunk = Chunk();  //creates an empty chunk using the constructor
+    
+                             //   AST Tree for code: let x = 5; print x;
+
+                             //          BlockStmt
+                             //         /         \
+                             //     VarDecl     PrintStmt
+                             //     (x = 5)     (print x)
+                             //         |            |
+                             //     LiteralExpr  VariableExpr
+                             //     (value: 5)   (name: x)
+
+                             // The compiler visits each node → generates bytecode for each
+    
+    if (ast) {   //does the AST exists
+        ast->accept(*this);    //if yes then start walking thru it and generate bytecode 
+    }
+    
+    //  Always add a 'RETURN' instruction at the very end so the VM knows to stop
+    emitByte(static_cast<uint8_t>(Opcode::OP_RETURN));
+    
+    
+    return currentChunk;
 }
 
-std::vector<uint8_t> Compiler::compile(ASTNode* ast) {
-    std::vector<uint8_t> bytecode;
-    // ---------------------------------------------------------
-    // TODO: Start traversing the AST from the root node.
-    // For each node, emit the corresponding opcodes.
-    // ---------------------------------------------------------
-    
-    // Example for a Binary Expression (e.g., 2 + 3):
-    // 1. Compile the left child (emits OP_CONSTANT 2).
-    // 2. Compile the right child (emits OP_CONSTANT 3).
-    // 3. Emit the operator opcode (OP_ADD).
-    
-    // Example for 'print x':
-    // 1. Compile the expression 'x' (emits OP_GET_VAR or OP_CONSTANT).
-    // 2. Emit OP_PRINT.
 
-    return bytecode;
-}
+
